2bafff5ab198f4ceaddab920171b51c717bd1290
Gerard Castellà <gerecaste91@gmail.com>
2020-02-25 10:36:23 +0100

    Add base prediction task to inherit from. Add logistic regression example.

---
+++ tasks/prediction/BasePredictionTask.py
@@ -0,0 +1,91 @@
+import pandas as pd
+from utils.read import read_split
+from utils.utils import evaluate_model
+import pickle as pk
+
+
+class BasePredictionTask:
+    """
+    This task implements prediction using the null model.
+    It is intended to be used for inheriting when using more complex models.
+    """
+
+    def __init__(self, run, config):
+        """
+        Init 3_prediction task.
+        """
+        self.run_str = run
+        self.config = config
+        self.split = read_split(config=config, run=run)
+        self.train_metric = []
+        self.test_metric = []
+        self.model_split = []
+
+    def run(self):
+        """
+        Main function.
+        This function can be changed when inheriting from this class.
+        """
+        target_str = self.config.prediction.target_var
+        for split in self.split:
+            print(f"Predictions for split {split['name']}.")
+
+            # Train
+            model = self.fit_model(df=split["train"])
+            self.model_split.append(model)
+
+            # Predictions
+            train_pred = self.model_predictions(df=split["train"], model=model)
+            test_pred = self.model_predictions(df=split["test"], model=model)
+            self.write_predictions(train_pred, f"train_pred_{split['name']}.csv")
+            self.write_predictions(test_pred, f"test_pred_{split['name']}.csv")
+
+            # Calculate metrics
+            self.train_metric.append(
+                evaluate_model(train_pred, split["train"][target_str])
+            )
+            self.test_metric.append(
+                evaluate_model(test_pred, split["test"][target_str])
+            )
+
+        print("Global model.")
+        full_data = pd.concat(self.split[0]["train"], self.split[0]["test"])
+        self.model = self.fit_model(df=full_data)
+        self.save_model()
+
+    def write_predictions(self, df: pd.DataFrame, name: str) -> str:
+        """
+        Write prediction file.
+        Args:
+            df: Data frame to be writen.
+            results_folder: Folder in config for 3_prediction.
+            name: string for the name of the folder with extension.
+        """
+        full_name = f"./{self.config.prediction.folder}/{self.run_str}/{name}"
+        print(full_name)
+
+        df.to_csv(full_name, index=False)
+
+    def fit_model(self, df: pd.DataFrame):
+        """
+        Fitting for the null model.
+        This function can be changed when inheriting from this class.
+        """
+        mode = df.groupby(self.config.prediction.target_var).count()["id"].idxmax()
+        return mode
+
+    def model_predictions(self, df: pd.DataFrame, model) -> pd.DataFrame:
+        """
+        Predictions for the null model.
+        This function can be changed when inheriting from this class.
+        """
+        df["prediction"] = model
+        return df[["id", "prediction"]]
+
+    def save_model(self):
+        folder = self.config.prediction.model.folder
+        label = self.config.prediction.model.label
+
+        pk.dump(self, open(f"{folder}/{self.run_str}_{label}.p", "wb"))
+
+        return 1
---
+++ tasks/prediction/__init__.py
@@ -0,0 +1,1 @@
+from .BasePredictionTask import BasePredictionTask
